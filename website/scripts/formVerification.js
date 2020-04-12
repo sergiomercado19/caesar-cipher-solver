804c11691f6faf452dbba634e51c99dddfa63055
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-04-12 23:26:24 +1000

    Remove quotes before sending plaintext/ciphertext to prevent JSON parsing issues

--- website/scripts/formVerification.js
+++ website/scripts/formVerification.js
@@ -4,6 +4,9 @@ export function encryptForm() {
    const filledPlaintext = plainboxText.value.length != 0;
    const filledShift = plainboxShift.value.length != 0;
 
+   // Remove quotes to prevent JSON parsing issues
+   plainboxText.value = plainboxText.value.replace(/['"`]/g, '');
+
    // Check relevant fields
    if (!filledPlaintext) plainboxText.classList.add("error-flash");
    if (!filledShift) plainboxShift.classList.add("error-flash");
@@ -19,6 +22,9 @@ export function decryptForm() {
    const filledCiphertext = cipherboxText.value.length != 0;
    const filledShift = cipherboxShift.value.length != 0;
 
+   // Remove quotes to prevent JSON parsing issues
+   cipherboxText.value = cipherboxText.value.replace(/['"`]/g, '');
+
    // Check relevant fields
    if (!filledCiphertext) cipherboxText.classList.add("error-flash");
    if (!filledShift) cipherboxShift.classList.add("error-flash");
@@ -32,6 +38,9 @@ export function decryptForm() {
 
 export function solveForm() {
    const filledCiphertext = cipherboxText.value.length != 0;
+
+   // Remove quotes to prevent JSON parsing issues
+   cipherboxText.value = cipherboxText.value.replace(/['"`]/g, '');
 
    // Check relevant fields
    if (!filledCiphertext) cipherboxText.classList.add("error-flash");
