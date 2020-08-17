4a2ae6dd5b476676b9f74a9077b2308a472f65c0
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-08-17 10:46:02 +1000

    Add comments to main go file

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+/*
+  Caesar Cipher Solver
+  --
+  This application has 3 main functions to interact with caesar ciphers:
+  - Solve
+  - Encrypt
+  - Decrypt
+*/
+
 package main
 
 import (
@@ -24,18 +33,20 @@ func handleEncrypt(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Request
+	// Process request
 	decoder := json.NewDecoder(r.Body)
 	var request payload
 	if err := decoder.Decode(&request); err != nil {
 		fmt.Fprintf(w, "Unable to parse request payload | err: %v", err)
 		return
 	}
+
+	// Encrypt: plaintext + shift => ciphertext
 	fmt.Printf("ENCRYPTING: plaintext received | payload = %v\n", request)
 	shift, _ := strconv.Atoi(request.Shift)
 	var ciphertext string = caesartools.Shift(request.Text, shift)
 
-	// Response
+	// Write response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"response": "` + ciphertext + `"}`))
@@ -47,18 +58,20 @@ func handleDecrypt(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Request
+	// Process request
 	decoder := json.NewDecoder(r.Body)
 	var request payload
 	if err := decoder.Decode(&request); err != nil {
 		fmt.Fprintf(w, "Unable to parse request payload | err: %v", err)
 		return
 	}
+
+	// Decrypt: ciphertext + shift => plaintext
 	fmt.Printf("DECRYPTING: ciphertext received | payload = %v\n", request)
 	shift, _ := strconv.Atoi(request.Shift)
 	var plaintext string = caesartools.Shift(request.Text, -1*shift)
 
-	// Response
+	// Write response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"response": "` + plaintext + `"}`))
@@ -70,18 +83,20 @@ func handleSolve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Request
+	// Process request
 	decoder := json.NewDecoder(r.Body)
 	var request payload
 	if err := decoder.Decode(&request); err != nil {
 		fmt.Fprintf(w, "Unable to parse request payload | err: %v", err)
 		return
 	}
+
+	// Solve: ciphertext => plaintext
 	fmt.Printf("SOLVING: ciphertext received | payload = %v\n", request)
 	var shift int = caesartools.Solve(request.Text, dictionary)
 	var plaintext string = caesartools.Shift(request.Text, -1*shift)
 
-	// Response
+	// Write response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"response": "` + plaintext + `", "shift": "` + strconv.Itoa(shift) + `"}`))
@@ -90,7 +105,10 @@ func handleSolve(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
+	// Serve page
 	mux.Handle("/", http.FileServer(http.Dir("./website")))
+
+	// Serve API endpoints
 	mux.HandleFunc("/encrypt", handleEncrypt)
 	mux.HandleFunc("/decrypt", handleDecrypt)
 	mux.HandleFunc("/solve", handleSolve)
@@ -99,6 +117,7 @@ func main() {
 	dictionary = make(map[string]int)
 	caesartools.ParseWords(dictionary)
 
+	// Initialize server with rate limiting
 	fmt.Println("Listening on port 8090")
 	log.Fatal(http.ListenAndServe(":8090", ratelimiter.Limit(mux)))
 }
